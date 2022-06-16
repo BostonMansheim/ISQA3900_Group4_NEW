b3f551965d62e358aa6e895bb10202473d84c8c9
BostonMansheim <106132026+BostonMansheim@users.noreply.github.com>
2022-06-15 21:56:32 -0500

    New Project

---
+++ ISQA3900_Group4_NEW/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ISQA3900_Group4_NEW project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ISQA3900_Group4_NEW.settings')
+
+application = get_asgi_application()
