b8ad40e5bbed9b999bc55ba7791fe71dcd9b7f09
BostonMansheim <106132026+BostonMansheim@users.noreply.github.com>
2022-06-17 21:12:06 -0500

    system changes???
    Don't know how these files got added. They mostly appear to be cache changes. Will revert if breaks things.

--- ISQA3900_Group4_NEW/settings.py
+++ ISQA3900_Group4_NEW/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'burgerwars.apps.BurgerwarsConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'ISQA3900_Group4_NEW.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
