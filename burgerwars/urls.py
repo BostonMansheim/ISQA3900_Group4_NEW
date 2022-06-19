0dc11559e803672e4e598dcf631817887b10d2bc
jacobfrisbie <jacobfrisbie@outlook.com>
2022-06-19 12:00:17 -0500

    About Page
    About page push

--- burgerwars/urls.py
+++ burgerwars/urls.py
@@ -6,4 +6,5 @@ app_name = 'burgerwars'
 urlpatterns = [
     path('', views.home, name='home'),
     re_path(r'^home/$', views.home, name='home'),
+    path('about', views.about, name='about'),
 ]
--- burgerwars/views.py
+++ burgerwars/views.py
@@ -2,6 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render#, get_object_or_404
 from django.shortcuts import redirect
 # Create your views here.
+
+
 def home(request):
     return render(request, 'burgerwars/home.html',
                   {'burgerwars': home})
+
+
+def about(request):
+    return render(request, 'burgerwars/about.html', {'burgerwars': about})
