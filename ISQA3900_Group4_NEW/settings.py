ec722147f5e4af775b9c3c927117a374ab8587bc
BostonMansheim <106132026+BostonMansheim@users.noreply.github.com>
2022-07-01 17:14:28 -0500

    Revert "Update settings.py"
    This reverts commit c6d9a994429df3178ea3240588c2ff1f3cfdf637.

--- ISQA3900_Group4_NEW/settings.py
+++ ISQA3900_Group4_NEW/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = "/home/bostonmansheim/myproject/static/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "burgerwars" / "static" / "images"
 
 LOGIN_REDIRECT_URL = '/'
