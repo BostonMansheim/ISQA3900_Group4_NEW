07bf77b326f074f0f9e97fbb07648195881762c0
jacobfrisbie <jacobfrisbie@outlook.com>
2022-06-30 19:08:50 -0500

    WIP Send Email
    Work in Progress upload to send email

--- ISQA3900_Group4_NEW/settings.py
+++ ISQA3900_Group4_NEW/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-)v(r!vufo8gna$d*h6#r$f1%z)s5o=m6^)4r1s01f(5wi24=0q
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
-
 
 # Application definition
 
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ISQA3900_Group4_NEW.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -112,7 +107,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -130,4 +124,3 @@ MEDIA_ROOT = BASE_DIR / "burgerwars" / "static" / "images"
 LOGIN_REDIRECT_URL = '/'
 
 LOGIN_URL = 'login'
-
--- burgerwars/views.py
+++ burgerwars/views.py
@@ -8,6 +8,9 @@ from .forms import *
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.db.models.query import *
+from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 # Create your views here.
 
 
@@ -64,4 +67,20 @@ class itemdetails(generic.DetailView):
 
 def confirmation(request):
     return render(request, 'burgerwars/confirmation.html')
+
+
+def contact(request):
+    form = ContactForm(request.POST)
+    if form.is_valid():
+        subject = form.cleaned_data['subject']
+        message = form.cleaned_data['message']
+        sender = form.cleaned_data['sender']
+        cc = form.cleaned_data['cc']
+        recipients = ['sender@example.com']
+        recipients.append(sender)
+        send_mail(subject, message, sender, recipients)
+        return HttpResponseRedirect('/thanks/')
+    else:
+        form = ContactForm()
+        return render(request,'/templates/checkout.html',{'form':form})
 
